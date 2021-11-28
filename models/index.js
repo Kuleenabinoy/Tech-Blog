93b5f973629874d1ee4cc0fefecc6170cf117dcf
leena <annachandy123@gmail.com>
2021-11-28 11:37:26 +1100

    views

--- models/index.js
+++ models/index.js
@@ -1,21 +1,57 @@
+// const User = require("./User");
+// const Post = require("./Post");
+// const Comment = require("./Comment");
+// User.hasMany(Post, {
+//     foreignKey: "userId",
+//     onDelete: "CASCADE",
+// });
+// Post.belongsTo(User, {
+//     foreignKey: "userId",
+// });
+// User.hasMany(Comment, {
+//     foreignKey: "userId",
+//     onDelete: "CASCADE",
+// });
+// Comment.belongsTo(User, {
+//     foreignKey: "userId",
+// });
+// Comment.belongsTo(Post, {
+//     foreignKey: "postId",
+// });
+// module.exports = { User, Comment, Post };
 const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
+
 User.hasMany(Post, {
     foreignKey: "userId",
     onDelete: "CASCADE",
 });
-Post.belongsTo(User, {
-    foreignKey: "userId",
-});
+
 User.hasMany(Comment, {
     foreignKey: "userId",
     onDelete: "CASCADE",
 });
+
+Post.belongsTo(User, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "postId",
+    onDelete: "CASCADE",
+});
+
 Comment.belongsTo(User, {
     foreignKey: "userId",
 });
+
 Comment.belongsTo(Post, {
     foreignKey: "postId",
 });
-module.exports = { User, Comment, Post };
+module.exports = {
+    User,
+    Comment,
+    Post,
+};
